241aa53d92585721318afcd8cb14bcd3f76b3fe2
AdiMondal <144584127+AdiMondal@users.noreply.github.com>
2024-02-03 16:56:43 +0530

    intro page semi done

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,26 @@
-adi
+<script>
+
+</script>
+<main class="flex flex-col flex-1 p-4">
+    <section id="intropage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+    <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-18">
+        <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="poppins text-violet-400">Adi</span> Mondal<br/>
+        Front End<span class="poppins text-violet-400"> Developer</span>
+        </h2>
+        <p class="text-base sm:text-lg md:text-xl">
+            My <span class="text-violet-400">favourite tech </span>includes JavaScript(REACT.JS or SvelteKit) and TailwindCSS
+        </p>
+        <button
+         class="blueShadow mx-auto lg:mr-auto text-base md:text-xl poppins relative overflow hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+         href="https://www.linkedin.com/in/adi-mondal-a39a28283/"
+         target="_blank"
+        >
+        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
+        <h4 class="relative z-9">
+         Get in touch &rarr;
+        </h4>
+        </button>
+    </div>
+    </section>
+</main>
